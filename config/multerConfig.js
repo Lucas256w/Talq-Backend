56ad4aef0b55a8257ac4acab4a94bc699965522d
Jiahao <lucashuamani256w@gmail.com>
2024-04-03 19:50:02 -0400

    Adjust the path for multer

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 // storing image on server's disk with multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/images");
+    cb(null, "../public/images");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
